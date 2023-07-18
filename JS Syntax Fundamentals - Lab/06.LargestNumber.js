b1c67e6fab019cbeaebc762b0b60ec7d63d6dc28
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-07-18 16:32:41 +0100

    Add files via upload

---
+++ JS Syntax Fundamentals - Lab/06.LargestNumber.js
@@ -0,0 +1,19 @@
+function largestNumber(num1,num2,num3){
+    let result;
+     
+    if(num1 > num2 && num1 > num3){
+        result = num1;
+    }
+
+    if(num2 > num1 && num2 > num3){
+        result = num2;
+    }
+
+    if(num3 > num2 && num3 > num1){
+        result = num3;
+    }
+
+    console.log(`The largest number is ${result}.`);
+}
+
+largestNumber(5,-3,16);
