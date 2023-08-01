28fda7deaa960430f25a5dd34873ffdd8da19e1e
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-08-01 20:14:05 +0100

    Add files via upload

---
+++ JS Syntax Fundamentals - Exercises/10. SameNumbers.js
@@ -0,0 +1,27 @@
+function sameNumbers(number) {
+    let digitToString = number.toString();
+    let sum = 0;
+    let isDifferent = false;
+
+    for (let index = 0; index < digitToString.length; index++) {
+
+        let firstSymbol = digitToString[0];
+
+        if(firstSymbol !== digitToString[index]){
+            isDifferent = true;
+        }
+
+        sum += Number (digitToString[index]);
+    }
+
+    if(isDifferent){
+        console.log("false")
+       }else{
+           console.log("true")
+       }
+    console.log(sum);
+
+}
+
+sameNumbers(1234);
+sameNumbers(22222);
