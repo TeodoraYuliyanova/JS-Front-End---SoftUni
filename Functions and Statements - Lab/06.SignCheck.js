2c29be04dacffd4b983d6aa0f27acdae71e84dfc
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-08-04 16:58:33 +0100

    Add files via upload

---
+++ Functions and Statements - Lab/06.SignCheck.js
@@ -0,0 +1,9 @@
+function signCheck(...numbers) {
+  
+    let sumNegative = 0;
+ 
+    numbers.forEach(element => {if (element < 0) sumNegative++ });
+ 
+ 
+   console.log(sumNegative % 2 !== 0 ? "Negative" : "Positive" );
+ }
