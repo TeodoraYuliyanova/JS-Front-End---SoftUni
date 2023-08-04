75c05203d15663148fdb57a8d3a70bc08dd5fca9
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-08-04 16:57:39 +0100

    Add files via upload

---
+++ Functions and Statements - Lab/03.RepeatString.js
@@ -0,0 +1,10 @@
+function repeatString(word, repeatCount) {
+
+    let output = "";
+
+    for (let index = 0; index < repeatCount; index++) {
+        output += word;
+    }
+
+    return output;
+}
