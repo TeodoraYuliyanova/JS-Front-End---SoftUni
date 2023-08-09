c959e8ae2cfa8ed256b7c4bacb56e97182c9556e
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-08-09 15:01:10 +0100

    Add files via upload

---
+++ Objects and Classes - Exercises/05.Inventory.js
@@ -0,0 +1,27 @@
+function createRegisterForHeroes(input){
+
+    const heroesRegister = [];
+
+    for (let line of input) {
+        let [hero,level,items] = line.split(" / ");
+
+        level = Number(level);
+
+        heroesRegister.push({hero,level,items});
+    }
+
+    const sortedHeroes = heroesRegister.sort((heroA,heroB) => heroA.level - heroB.level);
+
+    for (const {hero, level, items} of sortedHeroes) {
+        console.log(`Hero: ${hero}`);
+        console.log(`level => ${level}`);
+        console.log(`items => ${items}`);
+    }
+
+}
+
+createRegisterForHeroes([
+'Isacc / 25 / Apple, GravityGun',
+'Derek / 12 / BarrelVest, DestructionSword',
+'Hes / 1 / Desolator, Sentinel, Antara'
+]);
