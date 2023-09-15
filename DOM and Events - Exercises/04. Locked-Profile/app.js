4c0c85220b4ddc6b0b880d732f746092a00c755a
Teodora Yuliyanova <teodorayuliyanova@gmail.com>
2023-09-15 15:52:43 +0100

    Add files via upload

---
+++ DOM and Events - Exercises/04. Locked-Profile/app.js
@@ -0,0 +1,23 @@
+function lockedProfile() {
+    const buttons = Array.from(document.querySelectorAll("button"));
+  
+    buttons.forEach((button) => {
+      button.addEventListener("click", handleClick);
+    });
+    
+    function handleClick(e) {
+      const lockedRadioButton = e.currentTarget.parentElement.querySelector('input[type="radio"]');
+
+      if (lockedRadioButton.checked) {
+        return;
+      }
+
+      const hiddenInfo = e.target.parentElement.querySelector("div");
+
+      hiddenInfo.style.display =
+        hiddenInfo.style.display === "block" ? "none" : "block";
+        
+      e.currentTarget.textContent =
+        e.currentTarget.textContent === "Show more" ? "Hide it" : "Show more";
+    }
+  }
